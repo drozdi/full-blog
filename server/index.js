a4f9cdba01b2147821056514691b29de60c13b20
Drozdi <drozdi-klim@yandex.ru>
2024-12-12 22:52:54 +0300

    -

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const chalk = require('chalk');
 const cors = require('cors');
 const mongoose = require('mongoose');
